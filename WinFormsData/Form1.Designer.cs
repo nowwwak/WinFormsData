ae6ef7c3f2fc81d6351880d5205b02f3033f2888
tomasz.nowak <tomasz.nowak@PLWAWN291010>
2017-11-16 09:46:17 +0100

    Add project files.

---
+++ WinFormsData/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace WinFormsData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.CategoryToolStripComboBox = new System.Windows.Forms.ToolStripComboBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Label7 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.OrderTextBox = new System.Windows.Forms.TextBox();
+            this.DiscontinuedCheckBox = new System.Windows.Forms.CheckBox();
+            this.StockTextBox = new System.Windows.Forms.TextBox();
+            this.PriceTextBox = new System.Windows.Forms.TextBox();
+            this.QuantityTextBox = new System.Windows.Forms.TextBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.ProductsListBox = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.ProductsDataGridView = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.CategoryToolStripComboBox});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(503, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(58, 22);
+            this.toolStripLabel1.Text = "Category:";
+            // 
+            // CategoryToolStripComboBox
+            // 
+            this.CategoryToolStripComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CategoryToolStripComboBox.Name = "CategoryToolStripComboBox";
+            this.CategoryToolStripComboBox.Size = new System.Drawing.Size(121, 25);
+            this.CategoryToolStripComboBox.SelectedIndexChanged += new System.EventHandler(this.CategoryToolStripComboBox_SelectedIndexChanged);
+            this.CategoryToolStripComboBox.Click += new System.EventHandler(this.CategoryToolStripComboBox_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(503, 352);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.Label7);
+            this.tabPage1.Controls.Add(this.Label6);
+            this.tabPage1.Controls.Add(this.Label5);
+            this.tabPage1.Controls.Add(this.Label4);
+            this.tabPage1.Controls.Add(this.Label3);
+            this.tabPage1.Controls.Add(this.Label1);
+            this.tabPage1.Controls.Add(this.OrderTextBox);
+            this.tabPage1.Controls.Add(this.DiscontinuedCheckBox);
+            this.tabPage1.Controls.Add(this.StockTextBox);
+            this.tabPage1.Controls.Add(this.PriceTextBox);
+            this.tabPage1.Controls.Add(this.QuantityTextBox);
+            this.tabPage1.Controls.Add(this.NameTextBox);
+            this.tabPage1.Controls.Add(this.ProductsListBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(495, 326);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Form";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Label7
+            // 
+            this.Label7.AutoSize = true;
+            this.Label7.Location = new System.Drawing.Point(175, 178);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(69, 13);
+            this.Label7.TabIndex = 38;
+            this.Label7.Text = "Discontinued";
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(175, 155);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(75, 13);
+            this.Label6.TabIndex = 37;
+            this.Label6.Text = "Units on Order";
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(175, 129);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(73, 13);
+            this.Label5.TabIndex = 36;
+            this.Label5.Text = "Units in Stock";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(175, 103);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(53, 13);
+            this.Label4.TabIndex = 35;
+            this.Label4.Text = "Unit Price";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(175, 77);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(87, 13);
+            this.Label3.TabIndex = 34;
+            this.Label3.Text = "Quantity Per Unit";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(175, 50);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(35, 13);
+            this.Label1.TabIndex = 33;
+            this.Label1.Text = "Name";
+            // 
+            // OrderTextBox
+            // 
+            this.OrderTextBox.Location = new System.Drawing.Point(281, 148);
+            this.OrderTextBox.Name = "OrderTextBox";
+            this.OrderTextBox.Size = new System.Drawing.Size(209, 20);
+            this.OrderTextBox.TabIndex = 32;
+            // 
+            // DiscontinuedCheckBox
+            // 
+            this.DiscontinuedCheckBox.AutoSize = true;
+            this.DiscontinuedCheckBox.Location = new System.Drawing.Point(281, 178);
+            this.DiscontinuedCheckBox.Name = "DiscontinuedCheckBox";
+            this.DiscontinuedCheckBox.Size = new System.Drawing.Size(15, 14);
+            this.DiscontinuedCheckBox.TabIndex = 31;
+            this.DiscontinuedCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // StockTextBox
+            // 
+            this.StockTextBox.Location = new System.Drawing.Point(281, 122);
+            this.StockTextBox.Name = "StockTextBox";
+            this.StockTextBox.Size = new System.Drawing.Size(209, 20);
+            this.StockTextBox.TabIndex = 30;
+            // 
+            // PriceTextBox
+            // 
+            this.PriceTextBox.Location = new System.Drawing.Point(281, 96);
+            this.PriceTextBox.Name = "PriceTextBox";
+            this.PriceTextBox.Size = new System.Drawing.Size(209, 20);
+            this.PriceTextBox.TabIndex = 29;
+            // 
+            // QuantityTextBox
+            // 
+            this.QuantityTextBox.Location = new System.Drawing.Point(281, 70);
+            this.QuantityTextBox.Name = "QuantityTextBox";
+            this.QuantityTextBox.Size = new System.Drawing.Size(209, 20);
+            this.QuantityTextBox.TabIndex = 28;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(281, 44);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.ReadOnly = true;
+            this.NameTextBox.Size = new System.Drawing.Size(209, 20);
+            this.NameTextBox.TabIndex = 27;
+            // 
+            // ProductsListBox
+            // 
+            this.ProductsListBox.FormattingEnabled = true;
+            this.ProductsListBox.Location = new System.Drawing.Point(3, 6);
+            this.ProductsListBox.Name = "ProductsListBox";
+            this.ProductsListBox.Size = new System.Drawing.Size(161, 186);
+            this.ProductsListBox.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.ProductsDataGridView);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(495, 326);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Grid";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // ProductsDataGridView
+            // 
+            this.ProductsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ProductsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ProductsDataGridView.Location = new System.Drawing.Point(3, 3);
+            this.ProductsDataGridView.Name = "ProductsDataGridView";
+            this.ProductsDataGridView.Size = new System.Drawing.Size(489, 320);
+            this.ProductsDataGridView.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 377);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Data Binding";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView ProductsDataGridView;
+        private System.Windows.Forms.ListBox ProductsListBox;
+        private System.Windows.Forms.ToolStripComboBox CategoryToolStripComboBox;
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox OrderTextBox;
+        internal System.Windows.Forms.CheckBox DiscontinuedCheckBox;
+        internal System.Windows.Forms.TextBox StockTextBox;
+        internal System.Windows.Forms.TextBox PriceTextBox;
+        internal System.Windows.Forms.TextBox QuantityTextBox;
+        internal System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    }
+}
+
---
+++ WinFormsData/Form1.cs
@@ -0,0 +1,59 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsData
+{
+    public partial class Form1 : Form
+    {
+        private ObjectSource _source = new ObjectSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CategoryToolStripComboBox.ComboBox.DisplayMember = "CategoryName";
+            CategoryToolStripComboBox.ComboBox.ValueMember = "CategoryID";
+            CategoryToolStripComboBox.ComboBox.DataSource = _source.GetCategories();//!!!! Warning if you put this before setting .ValueMember than .ValueMember will not work properly and will return whole object and not just property                                  
+        }        
+
+        private void CategoryToolStripComboBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CategoryToolStripComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int catId = (int)CategoryToolStripComboBox.ComboBox.SelectedValue;
+            IList<Product> products = _source.GetProducts(catId);
+            ProductsListBox.DataSource = products;
+            ProductsListBox.DisplayMember = "ProductName";
+
+            ProductsDataGridView.DataSource = products;
+            ProductsListBox.DataSource = products;
+            ProductsListBox.DisplayMember = "ProductName";
+
+            NameTextBox.DataBindings.Clear();
+            NameTextBox.DataBindings.Add("Text", products, "ProductName");
+            QuantityTextBox.DataBindings.Clear();
+            QuantityTextBox.DataBindings.Add("Text", products, "QuantityPerUnit");
+            PriceTextBox.DataBindings.Clear();
+            PriceTextBox.DataBindings.Add("Text", products, "UnitPrice");
+            StockTextBox.DataBindings.Clear();
+            StockTextBox.DataBindings.Add("Text", products, "UnitsInStock");
+            OrderTextBox.DataBindings.Clear();
+            OrderTextBox.DataBindings.Add("Text", products, "UnitsOnOrder");
+            DiscontinuedCheckBox.DataBindings.Clear();
+            DiscontinuedCheckBox.DataBindings.Add("Checked", products, "Discontinued");
+        }
+    }
+}
